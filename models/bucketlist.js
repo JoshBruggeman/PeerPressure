411b474027ee18def0529aaf3514745d6e76fcae
mitalimn <mitalinaik@yahoo.com>
2017-02-07 17:22:09 -0600

    database

---
+++ models/bucketlist.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  var BucketItem = sequelize.define('BucketItem', {
+      title: DataTypes.STRING,
+      isAchieved: DataTypes.BOOLEAN
+  },
+
+  {
+    // We're saying that we want our BucketItem to have Posts
+    classMethods: {
+      associate: function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        BucketItem.belongsTo(models.User, {
+          foreignKey: {
+              allowNull: false
+            }
+        });
+
+      }
+    }
+  }//classMethods
+);
+  return BucketItem;
+}
---
+++ models/user.js
@@ -0,0 +1,27 @@
+var bcrypt   = require('bcrypt-nodejs');
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define('User', {
+        username: DataTypes.STRING,
+        password: DataTypes.STRING
+    },
+    {
+    // We're saying that we want our Author to have Posts
+    classMethods: {
+      associate: function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        User.hasMany(models.BucketItem, {
+          onDelete: "cascade"
+        });
+      }
+    }
+  }
+  );
+    // generating a hash
+    User.generateHash = function(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    }
+    // checking if password is valid
+    User.validPassword = function(user, password) {
+        return bcrypt.compareSync(password, user.password);
+    };
