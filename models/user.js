4e949c4d696b0576f8307db018d0dc2c34def383
mitalimn <mitalinaik@yahoo.com>
2017-02-07 18:28:08 -0600

    index

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var bcrypt   = require('bcrypt-nodejs');
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define('User', {
-        username: DataTypes.STRING,
+        email: DataTypes.STRING,
         password: DataTypes.STRING
     },
     {
@@ -25,10 +25,6 @@ module.exports = function(sequelize, DataTypes) {
     User.validPassword = function(user, password) {
         return bcrypt.compareSync(password, user.password);
     };
-
-
     sequelize.sync({force: true});
-
-
     return User;
 }
